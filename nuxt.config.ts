3b1b64a778ada0bccfd6a32abfc7802b64270f28
Edzel <dev.edzeldacanay@gmail.com>
2023-07-12 10:35:09 +0800

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'EGGY PAY',
+      meta: [
+          { charset: 'utf-8'},
+          {name: '', content}
+      ],
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  } 
+})
