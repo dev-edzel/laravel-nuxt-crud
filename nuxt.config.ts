8b1849a6627bc21e190ed0a9f0253e4c0f767416
Edzel <dev.edzeldacanay@gmail.com>
2023-07-12 10:46:34 +0800

    updated config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,35 @@ export default defineNuxtConfig({
       title: 'EGGY PAY',
       meta: [
           { charset: 'utf-8'},
-          {name: '', content}
+          { name: 'viewport', content: 'width=device-width, initial-scale=1'},
+          { hid: 'description', name: 'description', content: 'NUXT meta'},
+          { name: 'format-detection', content: 'cellphone-no'}
       ],
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.icon'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+        }
+      ],
+      script: [
+        {
+          src: 'https://code.jquery.com/jquery-3.7.0.min.js',
+          type: 'text/javascript'
+        },
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+          type: 'text/javascript'
+        },
+      ],
     }
   } 
 })
